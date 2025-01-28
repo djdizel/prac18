459829b70f2606d04c979827d58faef7f1813ce2
djdizel <158520404+djdizel@users.noreply.github.com>
2025-01-28 14:25:16 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,12 +2,10 @@
 
 class Person
 {
-    // Поля
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public int Age { get; set; }
 
-    // Конструктор
     public Person(string firstName, string lastName, int age)
     {
         FirstName = firstName;
@@ -15,7 +13,6 @@ class Person
         Age = age;
     }
 
-    // Метод вывода данных
     public void PrintInfo()
     {
         Console.WriteLine($"Имя: {FirstName}, Фамилия: {LastName}, Возраст: {Age}");
@@ -26,10 +23,8 @@ class Program
 {
     static void Main()
     {
-        // Создание объекта
         Person person = new Person("Иван", "Иванов", 30);
         
-        // Вывод информации
         person.PrintInfo();
     }
 }
