1ad2a2ba0427fca668788eb8a7ea91ac44b83886
djdizel <158520404+djdizel@users.noreply.github.com>
2025-01-28 12:44:26 +0200

    123

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Person
+{
+    // Поля
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+    // Конструктор
+    public Person(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    // Метод вывода данных
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Имя: {FirstName}, Фамилия: {LastName}, Возраст: {Age}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Создание объекта
+        Person person = new Person("Иван", "Иванов", 30);
+        
+        // Вывод информации
+        person.PrintInfo();
+    }
+}
